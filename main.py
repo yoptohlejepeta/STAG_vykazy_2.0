77d977094f3a1f90f69d2155f8f74be1aa06cccf
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-06-03 23:54:01 +0200

    logfire

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import logfire
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+logfire.configure()
+logfire.instrument_fastapi(app)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
