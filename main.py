7546bae1da48059aa864e8fb7677f133ff55f65c
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-06-04 00:03:46 +0200

    logfire auth

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import logfire
+import os
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
-logfire.configure()
+logfire.configure(token=os.getenv("LOGFIRE_TOKEN"), project_name=os.getenv("LOGFIRE_PROJECT_NAME"))
 logfire.instrument_fastapi(app)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
