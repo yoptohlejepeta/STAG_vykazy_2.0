975e67849c0482cfdbcc88534c8980da01a94737
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-06-04 10:33:37 +0200

    test plot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import logfire
 import os
+import plotly.express as px
+import pandas as pd
+import random
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -18,4 +21,27 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    test_df = pd.DataFrame({
+        "x": random.sample(range(1, 200), 100),
+        "y": random.sample(range(1, 200), 100),
+    })
+    
+    fig = px.scatter(test_df, x="x", y="y", color_discrete_sequence=["#DB924B"], template="none")
+    fig.update_layout(
+        title="Test Plot",
+        xaxis_title="X",
+        yaxis_title="Y",
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="white"
+        ),
+        plot_bgcolor="rgba(0,0,0,0)",
+        paper_bgcolor="rgba(0,0,0,0)",
+        # hide grid
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
+    )
+
+    return templates.TemplateResponse("index.html", {"request": request, "plot": fig.to_html(full_html=False, config={"displayModeBar": False}
+)})
